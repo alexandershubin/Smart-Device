c699ff60886aa043596727f63052c1c31c400586
alexandershubin <shubin8@mail.ru>
2019-09-24 12:06:06 +0300

    локал для формы на странице

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,12 @@
 var link = document.querySelector(".header__right--button");
 var popup = document.querySelector(".modal");
+var formPage = document.querySelector(".form__login");
 var close = popup.querySelector(".modal__close");
 var login = popup.querySelector("[name=login]");
 var phone = popup.querySelector("[name=phone]");
-var form = popup.querySelector("form");
+var name = formPage.querySelector("[name=name]");
+var number = formPage.querySelector("[name=number]");
+var form = document.querySelectorAll("form");
 var overlay = document.querySelector(".overlay");
 var toggle = document.querySelector(".toggle");
 var toggleOffice = document.querySelector(".toggle-button");
@@ -15,7 +18,7 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-  storage = localStorage.getItem("login");
+  storage = localStorage.getItem("login", "name");
 } catch (err) {
   isStorageSupport = false;
 }
@@ -25,11 +28,11 @@ link.addEventListener("click", function(evt) {
   popup.classList.add("modal-show");
   overlay.classList.add("overlay-modal");
   if (storage) {
-   login.value = storage;
-    phone.focus();
-  } else {
-   login.focus();
-  }
+    login.value = storage;
+     phone.focus();
+   } else {
+    login.focus();
+   }
 });
 
 close.addEventListener("click", function(evt) {
@@ -72,17 +75,29 @@ overlay.addEventListener("click", function(evt) {
   overlay.classList.remove("overlay-modal");
 });
 
-
-form.addEventListener("submit", function (evt) {
+popup.addEventListener("submit", function (evt) {
   if (!login.value || !phone.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
+    formPage.offsetWidth = formPage.offsetWidth;
     popup.classList.add("modal-error");
     console.log("Нужно Имя и телефон");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", login.value);
+    }
+  }
+});
+
+formPage.addEventListener("submit", function (evt) {
+  if (!name.value || !number.value) {
+    evt.preventDefault();
+    formPage.offsetWidth = formPage.offsetWidth;
+    console.log("Нужно Имя и телефон");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
     }
   }
 });
